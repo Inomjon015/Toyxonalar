97186e354c5a38a6779a6f7810ce601e1517ea28
watch <asd@gmail.com>
2025-12-09 15:56:10 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1 className='bg-red-500'>sssssssss</h1>
+      <h1 className='bg-red-500'>salom</h1>
+      <h2>Abdusharif</h2>
     </div>
   )
 }
